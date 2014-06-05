be383df8cce48e39bd9fe7d4697070d31633a641
abcdjdj <abcdjdj@gmail.com>
2014-06-05 16:40:26 +0530

    carbon.mk : Don't inherit from common_gsm

--- carbon.mk
+++ carbon.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := p3100
 
